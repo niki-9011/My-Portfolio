af3fa0aba8c70aba5af819c2d9036928333e3035
niki9011 <niki901115@gmail.com>
2024-04-04 13:45:10 +0300

    slider pictures

--- app.js
+++ app.js
@@ -49,3 +49,13 @@ window.addEventListener('scroll', function () {
         mysBtn.style.display = 'none'; 
     } 
 }); 
+
+let slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+let slideInterval = setInterval(nextSlide, 2000);
+
+function nextSlide() {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add('active');
+}
