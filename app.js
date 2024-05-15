2c47e4081aec46c05917cf09005ca24fc50a6edd
Nikolay Videnov <niki901115@gmail.com>
2024-05-15 21:11:28 +0300

    animate the background image

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let tablinks = document.getElementsByClassName("tab-links");
 let tabcontents = document.getElementsByClassName("tab-contents");
 
+gsap.to("#header", { duration: 3, backgroundPosition: "50% 100%", ease: "power2.out", repeat: -1, yoyo: true });
+
 function opentab(tabname) {
     for (tablink of tablinks) {
         tablink.classList.remove("active-link");
@@ -216,6 +218,7 @@ class Slideshow {
 !function(e){"undefined"==typeof module?this.charming=e:module.exports=e}(function(e,n){"use strict";n=n||{};var t=n.tagName||"span",o=null!=n.classPrefix?n.classPrefix:"char",r=1,a=function(e){for(var n=e.parentNode,a=e.nodeValue,c=a.length,l=-1;++l<c;){var d=document.createElement(t);o&&(d.className=o+r,r++),d.appendChild(document.createTextNode(a[l])),n.insertBefore(d,e)}n.removeChild(e)};return function c(e){for(var n=[].slice.call(e.childNodes),t=n.length,o=-1;++o<t;)c(n[o]);e.nodeType===Node.TEXT_NODE&&a(e)}(e),e});
 
 const slideshow = new Slideshow(document.querySelector('.slideshow'));
+
 
 
 
